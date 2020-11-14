67da5aacddb27f3adc0f80e64fd871d2adc7ca3f
ShaharEli <shahar.e3@gmail.com>
2020-11-14 17:23:07 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app  = express()
 const http = require("http").createServer(app)
 const io = require("socket.io")(http)
-app.use(express.static("..client/build"))
+app.use(express.static("../client/build"))
 io.on("connection",(socket)=>{
     socket.on("message",(data)=>{
         io.emit("chat",(data))
