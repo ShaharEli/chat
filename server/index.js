22a3ec3bda0489a3b4ba63cd9bc9475a805b8047
ShaharEli <shahar.e3@gmail.com>
2020-11-16 21:33:11 +0200

    ignore update

--- server/index.js
+++ server/index.js
@@ -1,19 +1,22 @@
-const express = require("express")
-const app  = express()
-const http = require("http").createServer(app)
-const io = require("socket.io")(http)
-app.use(express.static("../client/build"))
-io.on("connection",(socket)=>{
-    socket.on("message",(data)=>{
-        io.emit("chat",(data))
-    })
-    socket.on("name",e=>{
-        io.emit("chat",({name:e+" - logged in",message:""}))
-    })
-})
+const express = require("express");
+const app = express();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
+const path = require("path");
+app.use(express.static("../client/build"));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+});
 
+io.on("connection", (socket) => {
+  socket.on("message", (data) => {
+    io.emit("chat", data);
+  });
+  socket.on("name", (e) => {
+    io.emit("chat", { name: e + " - logged in", message: "" });
+  });
+});
 
-
-http.listen(8282,()=>{
-    console.log("listening on port 8282");
-})
+http.listen(8282, () => {
+  console.log("listening on port 8282");
+});
