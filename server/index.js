caac19499f33bb89b9a938abfd373c4e73fd1fa0
Shahar Eliyahu <65499638+ShaharEli@users.noreply.github.com>
2020-11-16 21:43:48 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -17,6 +17,6 @@ io.on("connection", (socket) => {
   });
 });
 
-http.listen(8282, () => {
-  console.log("listening on port 8282");
+http.listen(8080, () => {
+  console.log("listening on port 8080");
 });
